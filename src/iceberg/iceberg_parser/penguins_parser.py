3e3a22e1b473001cbe667327511422cae1af8821
Aymen <aymenfadhel@yahoo.com>
2020-03-16 18:36:27 -0400

    fixing penguins_parser

--- src/iceberg/iceberg_parser/penguins_parser.py
+++ src/iceberg/iceberg_parser/penguins_parser.py
@@ -16,7 +16,7 @@ class PenguinsSubparser(object):
             penguins_parser.set_defaults(which='penguins'),
             penguins_parser.add_argument('--gpu_ids', '-gid',
                                          help='The GPU id 0 or 1'),
-            penguins_parser.add_argument('--epoch', 'ep',
+            penguins_parser.add_argument('--epoch', '-ep',
                                          help='The number of epoches default is \
                                          set to 300'),
             penguins_parser.add_argument('--model', '-m',
